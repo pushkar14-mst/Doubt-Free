d437a71251cd2c06c4b82b369947d5e9937c6013
Parth Pitrubhakta <parthwani1990@gmail.com>
2023-02-18 23:23:31 +0530

    Implemented Landing Page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-mongoose.connect("mongodb://localhost:27017/blogsDB");
+mongoose.connect("mongodb://localhost:27017/doubtsDB");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
