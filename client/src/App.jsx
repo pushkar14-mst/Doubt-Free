dbeaa22ccac2b0501a5f86224e54d48ee2b14928
Parth Pitrubhakta <parthwani1990@gmail.com>
2023-02-18 23:32:10 +0530

    Merged the changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,7 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Route, Routes } from "react-router";
 
 import './App.css';
-import SearchBox from './Components/SearchBox/SearchBox';
-import teacher from './images/teacher.png';
-import explore from './images/explore.png'
-import learn from './images/learn.png'
-import connect from './images/connect.png'
-import glass from './images/glass.svg';
+
 import DoubtPage from "./pages/DoubtPage";
 import HomePage from "./pages/HomePage";
 
@@ -32,48 +27,6 @@ function App() {
       <Route path="/doubts" />
       <Route path="/doubts/:doubtId" element={<DoubtPage />} />
     </Routes>
-    <div className='App'>
-      <header className='App-header'>
-        <Container>
-          <Row className='hero-section'>
-            <Col lg={6} md={12} sm={12}>
-
-              <div class="mb-5 mb-lg-0 text-center text-lg-start">
-                <h1 class="display-1 lh-1 mb-3 d-flex "
-                  style={{ fontSize: "80px", fontWeight: "650" }}>
-                  Welcome to Doubt-Free
-                </h1>
-                <p class="lead fw-normal text-muted mb-5">
-                  A website for finding and exploring new things.
-                </p>
-                <input onChange={e => setQuery(e)} onKeyPress={e => handleKey(e)} autoFocus={true} />
-                <img className="glass" alt="magnifying glass" src={glass} style={{ width: '30px', height: '30px' }} />
-              </div>
-            </Col>
-            <Col lg={6} md={12} sm={12}>
-              <img src={teacher} alt="Teacher Image" style={{ width: '450px', height: '450px' }} />
-             </Col>
-          </Row>
-          <Row className="feature-section">
-             <Col lg={4} md={12} sm={12}>
-              <img src={explore} alt="Teacher Image" style={{ width: '150px', height: '150px' }} />
-               <h2>Find new domains</h2>
-               <p>Explore the tech world and discover domains to learn.</p>
-             </Col>
-             <Col lg={4} md={12} sm={12}>
-              <img src={learn} alt="Teacher Image" style={{ width: '150px', height: '150px' }} />
-               <h2>Try new things</h2>
-               <p>Learn new skills and try new experiences.</p>
-             </Col>
-             <Col lg={4} md={12} sm={12}>
-              <img src={connect} alt="Teacher Image" style={{ width: '150px', height: '150px' }} />
-               <h2>Connect with others</h2>
-               <p>Find like-minded people and share your experiences.</p>
-             </Col>
-           </Row>
-        </Container>
-      </header>
-    </div>
   );
 }
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,6 +5,13 @@ import { doubtsActions } from "../store/doubts-store";
 import "./HomePage.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import SearchBox from './Components/SearchBox/SearchBox';
+import teacher from './images/teacher.png';
+import explore from './images/explore.png'
+import learn from './images/learn.png'
+import connect from './images/connect.png'
+import glass from './images/glass.svg';
+
 const HomePage = (props) => {
   const [name, setName] = useState("");
   const [doubtTitle, setDoubtTitle] = useState("");
@@ -38,6 +45,47 @@ const HomePage = (props) => {
   return (
     <>
       <NavBar />
+      <section>
+        <Container>
+          <Row className='hero-section'>
+            <Col lg={6} md={12} sm={12}>
+
+              <div class="mb-5 mb-lg-0 text-center text-lg-start">
+                <h1 class="display-1 lh-1 mb-3 d-flex "
+                  style={{ fontSize: "80px", fontWeight: "650" }}>
+                  Welcome to Doubt-Free
+                </h1>
+                <p class="lead fw-normal text-muted mb-5">
+                  A website for finding and exploring new things.
+                </p>
+                <input onChange={e => setQuery(e)} onKeyPress={e => handleKey(e)} autoFocus={true} />
+                <img className="glass" alt="magnifying glass" src={glass} style={{ width: '30px', height: '30px' }} />
+              </div>
+            </Col>
+            <Col lg={6} md={12} sm={12}>
+              <img src={teacher} alt="Teacher Image" style={{ width: '450px', height: '450px' }} />
+            </Col>
+          </Row>
+          <Row className="feature-section">
+            <Col lg={4} md={12} sm={12}>
+              <img src={explore} alt="Teacher Image" style={{ width: '150px', height: '150px' }} />
+              <h2>Find new domains</h2>
+              <p>Explore the tech world and discover domains to learn.</p>
+            </Col>
+            <Col lg={4} md={12} sm={12}>
+              <img src={learn} alt="Teacher Image" style={{ width: '150px', height: '150px' }} />
+              <h2>Try new things</h2>
+              <p>Learn new skills and try new experiences.</p>
+            </Col>
+            <Col lg={4} md={12} sm={12}>
+              <img src={connect} alt="Teacher Image" style={{ width: '150px', height: '150px' }} />
+              <h2>Connect with others</h2>
+              <p>Find like-minded people and share your experiences.</p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
       <section id="add-your-doubt">
         <div className="container">
           <div className="row">
